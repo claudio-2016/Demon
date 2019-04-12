5f2e59456c0d901fe3e3ce4465b203d8a648f838
claudio-2016 <arguello.fcs@gmail.com>
2019-04-12 19:42:19 -0300

    Up!!

--- install.sh
+++ install.sh
@@ -10,11 +10,10 @@ sudo apt -y install fonts-dejavu-extra fonts-dejavu xfce4-terminal
 clear
 sudo apt -y install thunar iceweasel iceweasel-l10n-es-ar alsa-utils 
 clear
-sudo apt install -y lxappearance lxappearance-obconf numix-gtk-theme numix-icon-theme
-clear
-sudo apt install -y lightdm lightdm-gtk-greeter
+sudo apt install -y lxappearance lxappearance-obconf numix-gtk-theme numix-icon-theme slim
 clear
 cp -R .config  ~/
 echo 'exec openbox-session' > ~/.xinitrc
 clear
-echo 'El proceso ha finalizado, si se encuentra dentro de VirtualBox, por favor corra el script vBox.sh como root. Para tener soperte completo dentro de la virtual machine. Recuerde hacer ejecutable el script con (chmod +x vBox.sh)'
+echo -e 'El proceso ha finalizado, si se encuentra dentro de VirtualBox, por favor corra el script vBox.sh como root. 
+\nPara tener soperte completo dentro de la virtual machine. \nRecuerde hacer ejecutable el script con (chmod +x vBox.sh)'
