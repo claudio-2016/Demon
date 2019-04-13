a27c15313da1acf1a819a1d7731e3363a219298b
claudio-2016 <arguello.fcs@gmail.com>
2019-04-13 20:50:46 -0300

    Up!!

--- install.sh
+++ install.sh
@@ -6,14 +6,13 @@ sudo apt install -y xorg
 clear
 sudo apt install -y openbox obconf rofi tint2
 clear
-sudo apt -y install fonts-dejavu-extra fonts-dejavu xfce4-terminal 
+sudo apt -y install fonts-dejavu-extra fonts-dejavu xfce4-terminal htop
 clear
 sudo apt -y install thunar iceweasel iceweasel-l10n-es-ar alsa-utils 
 clear
-sudo apt install -y lxappearance lxappearance-obconf numix-gtk-theme numix-icon-theme slim
+sudo apt install -y lxappearance lxappearance-obconf numix-gtk-theme numix-icon-theme network-manager-gnome
 clear
 cp -R .config  ~/
 echo 'exec openbox-session' > ~/.xinitrc
 clear
-echo -e 'El proceso ha finalizado, si se encuentra dentro de VirtualBox, por favor corra el script vBox.sh como root. 
-\nPara tener soperte completo dentro de la virtual machine. \nRecuerde hacer ejecutable el script con (chmod +x vBox.sh)'
+echo -e 'El proceso ha finalizado, si se encuentra dentro de VirtualBox, por favor \ncorra el script vBox.sh como usuario root. Para tener soperte completo dentro de la virtual machine. \nRecuerde hacer ejecutable el script con (chmod +x vBox.sh)'
