19e99bdac3f25fa7e205ff37af20bf8214839c3c
claudio-2016 <arguello.fcs@gmail.com>
2019-04-19 02:07:34 -0300

    Up!!!

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+## Instalacion de paquetes basicos
 sudo apt install -y ufw && ufw enable
 clear
 sudo apt install -y xorg
@@ -12,7 +13,26 @@ sudo apt -y install thunar iceweasel iceweasel-l10n-es-ar alsa-utils
 clear
 sudo apt install -y lxappearance lxappearance-obconf numix-gtk-theme numix-icon-theme network-manager-gnome
 clear
+sudo apt install -y mate-power-manager volumeicon-alsa
+clear
+
+
+## Configuracion basica para OpenBox
 cp -R .config  ~/
 echo 'exec openbox-session' > ~/.xinitrc
 clear
+
+
+## Modificacion del tiempo de Grub
+sudo cp /etc/default/grub /etc/default/grub.original
+sudo sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+clear
+
+
+## Se elminan las ultimas lineas del archivo interfaces para que se pueda configurar o conectarse a redes wifi, etc.
+sudo cp /etc/network/interfaces /etc/network/interfaces.original
+sudo sed -i '10,12d' /etc/network/interfaces
+
+
 echo -e 'El proceso ha finalizado, si se encuentra dentro de VirtualBox, por favor \ncorra el script vBox.sh como usuario root. Para tener soperte completo dentro de la virtual machine. \nRecuerde hacer ejecutable el script con (chmod +x vBox.sh)'
